8a4ae54e3b8a9bf56a7503745743f0714c3e6110
seong <sngoneee@gmail.com>
2023-09-18 22:19:06 +0900

    캘린더 추가

--- src/component/tabcont.js
+++ src/component/tabcont.js
@@ -5,7 +5,7 @@ import Todolist from "../page/Todolist"
 import { Memo } from "../page/Memo"
 import { Main } from '../page/main'
 import moment from 'moment';  //현재 시간 사용
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import 'moment/locale/ko';
 const today = moment().format('YYYY-MM-DD')
 
@@ -17,11 +17,24 @@ function Cont () {
 
 
   let bg = useSelector((state) => {return state.bg})
-  let tabidx = useSelector((state) => {return state.tabidx})
+  let tabidx = useSelector((state) => {return state.tabidx.id})
+  // const [curidx, setcurIdx] = useState(0)
   console.log("cont안의 idx " + tabidx)
 
 
+  // useEffect(() => {
+  //   setcurIdx(tabidx)
+  //   console.log("tabidx 함수 탐" + tabidx)
+  //   return () => {
+  //     console.log("cleanUp 함수");
+  //   };
+  // });
 
+  // useEffect
+  // const setTab = (tabidx) => {
+  //   setcurIdx(tabidx)
+  //   console.log("함수 탐")
+  // }
 
 
   return (
